f27e7b3d494c3d5cc3b3f1b8a5e005114d75facb
Elena Tikhonova <licose2019@gmail.com>
2022-05-09 02:01:05 +0300

    make it better

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -2,6 +2,12 @@ const { Images } = require("../models/models");
 const ApiError = require('../error/apiArror');
 const uuid = require('uuid')
 const path = require('path')
+const cloudinary = require('cloudinary')
+cloudinary.config({ 
+    cloud_name: 'da1qjbkf9', 
+    api_key: '124156134784274', 
+    api_secret: '6Ltajbxrev0dF77uLTpGOsJLAEg' 
+  });
 
 
 class ImagesController {
@@ -10,6 +16,10 @@ class ImagesController {
         const {scr} = req.files
         let fileName = uuid.v4() + ".jpg"
         scr.mv(path.resolve(__dirname, '..', 'static', fileName))
+        cloudinary.v2.uploader.upload(__dirname, '..', 'static', fileName,
+        { public_id: fileName }, 
+        function(error, result) {console.log(result); });
+        
         const image = await Images.create({productId, scr: fileName})
         return res.json(image)
     }
