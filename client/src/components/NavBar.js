5cc1b3808cfe6437a6c4708b2ed2cbea00fc9f5a
Elena Tikhonova <licose2019@gmail.com>
2022-05-09 18:35:55 +0300

    make it better

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -53,33 +53,35 @@ const NavBar = observer(() => {
             <Container>
                 <NavLink className="lable" style={{ color: 'black', marginLeft: '10px', textDecoration: 'none', marginRight: '20px', borderRadius: '50%' }} to={HOME_ROUTE}>MiniLi</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Nav className="mr-auto">
-                        <NavLink className="link" style={{ color: '#52527a', textDecoration: 'none', borderRadius: '5px' }} to={SHOP_ROUTE}>Каталог</NavLink>
-                        <NavLink className="link" style={{ color: '#52527a', textDecoration: 'none' }} to={ABOUT_ROUTE}>О Нас</NavLink>
-                        <NavLink className="link" style={{ marginLeft: '10px', color: '#52527a', textDecoration: 'none' }} to={HOWTO_ROUTE}>Как снять мерки</NavLink>
-                        <NavLink className="link" style={{ marginLeft: '10px', color: '#52527a', textDecoration: 'none' }} to={DELIVERY_ROUTE}>Оплата и доставка</NavLink>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Row className="justify-content-between">
+                        <Nav className="mr-2 ml-2 pl-2">
+                            <NavLink className="link" style={{ color: '#52527a', textDecoration: 'none', borderRadius: '5px' }} to={SHOP_ROUTE}>Каталог</NavLink>
+                            <NavLink className="link" style={{ color: '#52527a', textDecoration: 'none' }} to={ABOUT_ROUTE}>О Нас</NavLink>
+                            <NavLink className="link" style={{ color: '#52527a', textDecoration: 'none' }} to={HOWTO_ROUTE}>Как снять мерки</NavLink>
+                            <NavLink className="link" style={{ color: '#52527a', textDecoration: 'none' }} to={DELIVERY_ROUTE}>Оплата и доставка</NavLink>
 
-                    </Nav>
-                    <Nav>
-                        {user.isAuth ?
-                            <Nav className="ml-auto">
-                                <Button onClick={() => history.push(ADMIN_ROUTE)} variant={"outline-dark"}>Админ панель</Button>
-                                <Button onClick={() => logOut()} variant={"outline-dark"} className="mr-2 ml-2">Выйти</Button>
-                            </Nav>
-                            :
-                            <Nav className="ml-auto">
-                                <Button variant={"outline-dark"} onClick={() => history.push(LOGIN_ROUTE)} className="mr-2 ml-2">Log In</Button>
-                            </Nav>
-                        }
-                        <Nav className="ml-auto">
-                            <Button variant={"outline-dark"} onClick={() => clickCartHandler()}>Корзина</Button>
                         </Nav>
-                        <Nav className="ml-2">
-                            <Button variant={"outline-dark"} onClick={() => history.push(PROFILE_ROUTE)}>Профиль</Button>
+                        <Nav className="mr-2 ml-2 pl-2">
+                            {user.isAuth ?
+                                <Nav>
+                                    <Button className="mr-1" onClick={() => history.push(ADMIN_ROUTE)} variant={"outline-dark"}>Админ панель</Button>
+                                    <Button className="mr-1" onClick={() => logOut()} variant={"outline-dark"}>Выйти</Button>
+                                </Nav>
+                                :
+                                <Nav>
+                                    <Button className="mr-1" variant={"outline-dark"} onClick={() => history.push(LOGIN_ROUTE)}>Log In</Button>
+                                </Nav>
+                            }
+                            <Nav>
+                                <Button className="mr-1" variant={"outline-dark"} onClick={() => clickCartHandler()}>Корзина</Button>
+                            </Nav>
+                            <Nav>
+                                <Button className="mr-1" variant={"outline-dark"} onClick={() => history.push(PROFILE_ROUTE)}>Профиль</Button>
+                            </Nav>
                         </Nav>
-
-
-                    </Nav>
+                    </Row>
+                </Navbar.Collapse>
             </Container>
             <CartModal show={isCartOpen} onHide={() => setIsCartOpen(false)} />
         </Navbar>
