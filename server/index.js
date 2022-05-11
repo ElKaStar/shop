826c01b613436f310cc5d23d831f7a33840517e9
Elena Tikhonova <licose2019@gmail.com>
2022-05-11 20:20:16 +0300

    sep folders

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const PORT = process.env.PORT || 5000
+const cors = require('cors')
+const fileupload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandlingMiddleware = require('./middleware/errorHandlingMiddleware')
+const path = require('path')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileupload({}))
+app.use('/api', router)
+
+app.use(errorHandlingMiddleware)
+
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+
+        app.listen(PORT, () => console.log(`Server is started on port ${PORT} and ${process.env.IP}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
